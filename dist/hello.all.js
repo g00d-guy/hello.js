f6838eae9a3dd8746e69930c183cc5cfcf13aa59
Aleksey Aleksandrov <aleksandrov@bonustab.com>
2015-03-25 11:30:06 +0300

    vkontakte

--- dist/hello.all.js
+++ dist/hello.all.js
@@ -5317,6 +5317,23 @@ function arrayToDataResponse(res){
 //
 (function(hello){
 
+  function formatUser(o) {
+    if ( o.response && o.response[0].uid ) {
+      var user = o.response[0];
+      o.id = user.uid;
+      o.first_name = user.first_name;
+      o.last_name = user.last_name;
+      o.name = o.first_name + " " + o.last_name;
+      if ( user.photo_100 ) {
+        o.thumbnail = user.photo_100;
+      }
+      if ( user.bdate ) {
+        o.birthday = user.bdate;
+      }
+      delete o.response;
+    }
+  }
+
   function formatError(o) {
     if ( o.error ) {
       o.error = {
@@ -5331,8 +5348,8 @@ function arrayToDataResponse(res){
       name: 'VKontakte',
 
       login: function(p) {
-        p.options.window_width = 580;
-        p.options.window_height = 400;        
+        p.options.window_width = 630;
+        p.options.window_height = 500;        
       },
 
       oauth : {
@@ -5341,15 +5358,22 @@ function arrayToDataResponse(res){
         grant : 'https://oauth.vk.com/access_token'
       },
 
+      scope: {
+        basic:          '',
+        email:          'email',
+        offline_access: 'offline'
+      },
+
       base: 'https://api.vk.com/method/',
 
       get: {
-        'me': 'users.get'
+        'me': 'users.get?fields=photo_100,bdate,timezone'
       },
 
       wrap: {
         'me': function(res) {
           formatError(res);
+          formatUser(res);
           return res;
         }
       }
@@ -5661,6 +5685,82 @@ hello.init({
 
 })(hello);
 
+//
+// VKontakte
+//
+(function(hello){
+
+  function formatUser(o) {
+    console.dir(o);
+    /*if ( o.response && o.response[0].uid ) {
+      var user = o.response[0];
+      o.id = user.uid;
+      o.first_name = user.first_name;
+      o.last_name = user.last_name;
+      o.name = o.first_name + " " + o.last_name;
+      if ( user.photo_100 ) {
+        o.thumbnail = user.photo_100;
+      }
+      if ( user.bdate ) {
+        o.birthday = user.bdate;
+      }
+      delete o.response;
+    }*/
+  }
+
+  function formatError(o) {
+    if ( o.error ) {
+      o.error = {
+        code: o.error.error_code,
+        message: o.error.error_msg
+      };
+    }
+  }
+
+  function formatRequest(p, qs){
+    // Alter the querystring
+    var token = qs.access_token;
+    delete qs.access_token;
+    qs.oauth_token = token;
+    return true;
+  }
+
+  hello.init({
+    yandex: {
+      name: 'Yandex',
+
+      oauth : {
+        version : 2,
+        auth: 'https://oauth.yandex.ru/authorize',
+        grant: 'https://oauth.yandex.ru/token'
+      },
+
+      scope: {
+        basic:          '',
+        email:          'email',
+        offline_access: 'offline'
+      },
+
+      base: 'https://login.yandex.ru/',
+
+      get: {
+        'me': 'info?format=json'
+      },
+
+      wrap: {
+        'me': function(res) {
+          formatError(res);
+          formatUser(res);
+          return res;
+        }
+      },
+
+      xhr: formatRequest,
+      jsonp: formatRequest
+    }
+  });
+
+})(hello);
 //
 // AMD shim
 //
